f9526a7506e34617ee162eb8818df61e8a16a222
Fagundo <24-nfagundo@seaprep.org>
2023-06-19 12:05:14 -0700

    I want to commit these files so that I can add them to my repo!

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "My name is Nicolas Fagundo, I come from outerspace to conquer the human race.";
 	}
 
 }
